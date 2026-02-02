773e3299c7aa3ddf771f5ad14aa1f0c6c0b69789
yahiaaittaleb2005-svg <yahiaaittaleb2005@gmail.com>
2026-02-02 19:36:27 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,36 +1,27 @@
 import express from "express";
 import Stripe from "stripe";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
 const stripe = new Stripe(process.env.STRIPE_KEY);
 
-app.post("/checkout", async (req,res)=>{
-  const items = req.body.items;
-
+app.post("/create-checkout-session", async (req,res)=>{
   const session = await stripe.checkout.sessions.create({
     payment_method_types:["card"],
-    line_items: items.map(i=>({
+    line_items:[{
       price_data:{
         currency:"eur",
-        product_data:{ name:i.name },
-        unit_amount:i.price*100
+        product_data:{name:"Halal Life Planner"},
+        unit_amount:990
       },
       quantity:1
-    })),
+    }],
     mode:"payment",
-    success_url:"https://TUO-SITO.onrender.com/success.html",
-    cancel_url:"https://TUO-SITO.onrender.com/cancel.html"
+    success_url:"/success.html",
+    cancel_url:"/cancel.html"
   });
-
   res.json({url:session.url});
 });
 
-app.listen(3000, ()=> console.log("HalalHub online"));
+app.listen(3000);
